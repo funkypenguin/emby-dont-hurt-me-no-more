ef4be753b08774c217ce81c2054ff846fdcece74
David Young <davidy@funkypenguin.co.nz>
2018-12-11 08:38:38 +1300

    Use slicing to strip out HW decode

--- ffmpeg_wrapper.sh
+++ ffmpeg_wrapper.sh
@@ -6,20 +6,28 @@ REAL_FFMPEG=/bin/ffmpeg_for_realz
 # Do we have any stats on GPU memory free?
 if [ ! -f /gpu-free-ram ]
 then
-	# No stats, so just pass this through to the real FFMPEG
-	$REAL_FFMPEG "$@"
+        # No stats, so just pass this through to the real FFMPEG
+        $REAL_FFMPEG "$@"
 else
-	# Yes, we have stats. Now, do we have less than 1GB free GPU RAM?
-	FREE_GPU_RAM=`cat /gpu-free-ram`
+        # Yes, we have stats. Now, do we have less than 1GB free GPU RAM?
+        FREE_GPU_RAM=`cat /gpu-free-ram`
 
-	# If we have more than 1GB RAM, then all good, carry on
-	if [ "$FREE_GPU_RAM" -gt 1024 ]
-	then
-		$REAL_FFMPEG "$@"
-	else
-		# If the transcode matches a h264 decode, strip this out and allow the CPU to decode
-		ARGS=`echo $@ | sed -e 's/-c:v h264_cuvid -resize [[:digit:]]\+x[[:digit:]]\+//'` 
-		$REAL_FFMPEG $ARGS
-	fi
+        # If we have more than 1GB RAM, then all good, carry on
+        if [ "$FREE_GPU_RAM" -lt 1024 ]
+        then
+                # Are we doing a HW decode?
+                echo $@ | grep -q h264_cuvid
+
+                if [ $? -eq 0 ]
+                then
+                        # Yes - so strip off the first 16 characters of the ffmpeg command (removing the HW decode)
+                        $REAL_FFMPEG "${@:16:10000}"
+                else
+                        # No - proceed as normal
+                        $REAL_FFMPEG "$@"
+                fi
+        else
+                # Proceed as normal
+                $REAL_FFMPEG "$@"
+        fi
 fi
-
