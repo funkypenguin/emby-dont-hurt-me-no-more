3b6bbbe6ed629116d0c88d08c55a254a15cbbafa
David Young <davidy@funkypenguin.co.nz>
2018-12-11 21:02:35 +1300

    Add failsafe for extra load

--- ffmpeg_wrapper.sh
+++ ffmpeg_wrapper.sh
@@ -3,6 +3,11 @@
 # Would the _real_ ffmpeg please step forward?
 REAL_FFMPEG=/bin/ffmpeg_for_realz
 
+
+# What's our CPU usage?
+CPU_USAGE_FP=`cat /proc/loadavg | awk '{print $1}'`
+CPU_USAGE=`printf %.0f "$CPU_USAGE_FP"`
+
 # Do we have any stats on GPU memory free?
 if [ ! -f /gpu-free-ram ]
 then
@@ -15,6 +20,13 @@ else
         # If we have more than 1.5GB RAM, then all good, carry on
         if [ "$FREE_GPU_RAM" -lt 1500 ]
         then
+		# If load > 10 and we're running out of GPU RAM, then rather fail than take the whole node down
+		if [ "$CPU_USAGE" -gt 20 ]
+		then
+			echo "GPU free RAM < 1.5GB, but system load > 20. Aborting"
+			exit 1
+		fi
+		
                 # Are we doing a HW decode?
                 echo $@ | grep -q h264_cuvid
 
